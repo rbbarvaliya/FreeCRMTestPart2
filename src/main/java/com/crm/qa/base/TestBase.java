971ab0df394b2766256b59b16d850227969acea2
“rbbarvaliya” <ravindrabarvaliya@gmail.com>
2020-12-11 12:03:06 -0500

    commit after reaset

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -16,7 +16,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+<<<<<<< HEAD
 
+=======
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+>>>>>>> 463f99de87e260e1eec5c4a72fd50f6b5b25363e
 
 import com.crm.qa.util.TestUtil;
 import com.crm.qa.util.WebEventListener;
@@ -46,6 +51,7 @@ public class TestBase {
 
 	public static void initialization() throws IOException {
 		String browserName = prop.getProperty("browser");
+		System.out.println("Thred ID:" + Thread.currentThread().getId());
 		if (browserName.equals("chrome")){
 		    System.setProperty("webdriver.chrome.driver","C:\\Users\\ravin\\Downloads\\chromedriver.exe");
 		    driver = new ChromeDriver();
